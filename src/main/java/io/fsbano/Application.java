48c30823a1811b1bd1bf1d12407175be20d299ef
Fabio Sbano <fsbano@yahoo.com>
2022-06-11 12:24:30 -0300

    updated

--- src/main/java/io/fsbano/Application.java
+++ src/main/java/io/fsbano/Application.java
@@ -1,13 +1,15 @@
 package io.fsbano;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.cache.annotation.EnableCaching;
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @SpringBootApplication
-@EnableCaching(proxyTargetClass = true)
+@EnableAutoConfiguration
+@EnableAdminServer
 public class Application {
 
 	public static void main(String[] args) {
