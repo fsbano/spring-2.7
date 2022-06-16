7b68ab0b4f74bfe2e32c3351ebe5e4ae248fc8d2
Fabio Sbano <fsbano@yahoo.com>
2022-06-15 23:25:27 -0300

    Spring WebApplicationTest

--- src/test/java/io/fsbano/ContactControllerTest.java
+++ src/test/java/io/fsbano/ContactControllerTest.java
@@ -1,23 +1,47 @@
 package io.fsbano;
 
+import org.mockito.Mockito;
+
+import java.util.List;
+import java.util.ArrayList;
+
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
-@WebMvcTest(ContactController.class)
+import static org.hamcrest.Matchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.*;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@SpringBootTest
+@AutoConfigureMockMvc
 public class ContactControllerTest {
   @Autowired
-	private MockMvc mockMvc;
-
-  @MockBean
-	private ContactController controller;
-
+  private MockMvc mockMvc;
+  
   @Test
-  void whenCreateUser() throws Exception {
-
+  void createNewContact() throws Exception {
+     String url = "/api/contact";
+     mockMvc.perform(
+             post(url)
+             .accept("application/json")
+             .contentType("application/json")
+             .content("{ \"name\":\"Fabio Sbano\", \"email\":\"fsbano@yahoo.com\" }")
+            ).andExpect(status().isOk())
+            .andExpect(jsonPath("$.name", is("Fabio Sbano")));
   }
 }
